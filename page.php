9d3db625cdcd6a7e5a02f94f0f4c7056dae783e0
Cole Geissinger <colegeissinger@gmail.com>
2014-11-02 13:38:06 -0800

    Cleaning up some legacy code on the job board

--- page.php
+++ page.php
@@ -16,30 +16,20 @@ get_header(); ?>
 
 		<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php if ( is_page( 'job-board' ) && isset( $wp->query_vars['pagename'] ) && $wp->query_vars['pagename'] == 'jobs' ) : ?>
-				<a href="http://beawimp.org/jobs/add/" class="btn btn-primary pull-right" style="margin-top:23px;">Post a Job - $25 Limited Time Only</a>
+			<?php if ( is_page( 'jobs' ) ) : ?>
 				<h1>WIMP Job Board</h1>
 
-				<div class="jb-banner"><a href="http://beawimp.org/jobs/">View Jobs</a> <a href="http://beawimp.org/jobs/add/">Post A Job</a> <a href="http://beawimp.org/jobs/advanced-search/">Advanced Search</a></div>
-
-				<h2 class="subtitle">Browse Jobs by Category</h2>
-				<div class="row grid-squares">
-					<div class="box-square"><a href="http://beawimp.org/jobs/category/design/">Design</a></div>
-					<div class="box-square"><a href="http://beawimp.org/jobs/category/programing/">Programming</a></div>
-					<div class="box-square last"><a href="http://beawimp.org/jobs/category/business-management/">Business/Management</a></div>
-					<div class="box-square"><a href="http://beawimp.org/jobs/category/sales-business-dev/">Sales/Business Dev</a></div>
-					<div class="box-square"><a href="http://beawimp.org/jobs/category/education/">Education</a></div>
-					<div class="box-square last"><a href="http://beawimp.org/jobs/category/other/">Other</a></div>
-				</div>
-
-				<h2 class="subtitle">Search Job Listings</h2>
-
-			<?php elseif ( is_page( 'jobs' ) ) : ?>
-				<h1>WIMP Job Board</h1>
-
-				<div class="jb-banner"><a href="http://beawimp.org/jobs/">View Jobs</a> <a href="http://beawimp.org/jobs/add/">Post A Job</a> <a href="http://beawimp.org/jobs/advanced-search/">Advanced Search</a></div>
+				<div class="jb-banner"><a href="<?php echo esc_url( home_url( '/jobs/' ) ); ?>">View Jobs</a> <a href="<?php echo esc_url( home_url( '/jobs/add/' ) ); ?>">Post A Job</a> <a href="<?php echo esc_url( home_url( '/jobs/advanced-search/' ) ); ?>">Advanced Search</a></div>
+				<?php if ( 'add' === get_query_var( 'job_board' ) && ! is_user_logged_in() ) :
+					echo '<h1>Login Required!</h1><p>Please <a href="' . esc_url( home_url( '/members/sign-up/' ) ) . '">Sign up</a> for an account or use the login form above to add a job!</a></p>';
+				else:
+					get_template_part( 'content', get_post_type() );
+				endif; ?>
+			<?php else: ?>
+				<?php get_template_part( 'content', get_post_type() ); ?>
 			<?php endif; ?>
-			<?php get_template_part( 'content', get_post_type() ); ?>
+
+
 
 		<?php endwhile; ?>
 
