6af9c0d5284753df10e486ee3db1aefe164be4fe
Cole Geissinger <colegeissinger@gmail.com>
2014-11-02 16:07:52 -0800

    Adding data checks to the author block

--- partials/content-author-bio.php
+++ partials/content-author-bio.php
@@ -12,31 +12,47 @@ $author = wimp_get_author_data();
 	<?php if ( ! empty( $author->title ) || ! empty( $author->company ) ) : ?>
 		<h4 class="author-title"><?php echo esc_html( wimp_get_author_title() ); ?></h4>
 	<?php endif; ?>
+
 	<figure class="author-avatar">
 		<?php echo wp_kses_post( $author->avatar ); ?>
 	</figure>
 	<aside class="author-bio">
-		<?php echo wpautop( esc_html( $author->description ) ); ?>
+		<?php echo ( ! empty( $author->description ) ) ? wpautop( esc_html( $author->description ) ) : '<p>No bio set!</p>'; ?>
 
 		<ul class="author-social">
 			<li>
 				<a href="<?php echo esc_url( $author->wimp_url ); ?>" class="icon-profile" title="WIMP Profile"></a>
 			</li>
-			<li>
-				<a href="<?php echo esc_url( $author->facebook_url ); ?>" class="icon-facebook" title="Facebook Profile"></a>
-			</li>
-			<li>
-				<a href="<?php echo esc_url( $author->twitter_url ); ?>" class="icon-twitter" title="Twitter Profile"></a>
-			</li>
-			<li>
-				<a href="<?php echo esc_url( $author->linkedin_url ); ?>" class="icon-linkedin" title="LinkedIn Profile"></a>
-			</li>
-			<li>
-				<a href="<?php echo esc_url( $author->google_url ); ?>" class="icon-googleplus" title="Google+ Profile"></a>
-			</li>
-			<li>
-				<a href="<?php echo esc_url( $author->instagram_url ); ?>" class="icon-instagram" title="Instagram Profile"></a>
-			</li>
+
+			<?php if ( ! empty( $author->facebook_url ) ) : ?>
+				<li>
+					<a href="<?php echo esc_url( $author->facebook_url ); ?>" class="icon-facebook" title="Facebook Profile"></a>
+				</li>
+			<?php endif; ?>
+
+			<?php if ( ! empty( $author->twitter_url ) ) : ?>
+				<li>
+					<a href="<?php echo esc_url( $author->twitter_url ); ?>" class="icon-twitter" title="Twitter Profile"></a>
+				</li>
+			<?php endif; ?>
+
+			<?php if ( ! empty( $author->linkedin_url ) ) : ?>
+				<li>
+					<a href="<?php echo esc_url( $author->linkedin_url ); ?>" class="icon-linkedin" title="LinkedIn Profile"></a>
+				</li>
+			<?php endif; ?>
+
+			<?php if ( ! empty( $author->google_url ) ) : ?>
+				<li>
+					<a href="<?php echo esc_url( $author->google_url ); ?>" class="icon-googleplus" title="Google+ Profile"></a>
+				</li>
+			<?php endif; ?>
+
+			<?php if ( ! empty( $author->instagram_url ) ) : ?>
+				<li>
+					<a href="<?php echo esc_url( $author->instagram_url ); ?>" class="icon-instagram" title="Instagram Profile"></a>
+				</li>
+			<?php endif; ?>
 		</ul>
 	</aside>
 </div>
