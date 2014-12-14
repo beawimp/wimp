c8366326925055360d4e3f081353bc93a7774dcf
Cole Geissinger <colegeissinger@gmail.com>
2014-12-14 12:32:05 -0800

    Updating sidebar to reflect new changes to the site

--- sidebar.php
+++ sidebar.php
@@ -8,31 +8,24 @@ global $user_ID;
 get_currentuserinfo();
 ?>
 <section class="sidebar hidden-sm hidden-xs">
-	<aside class="wimp-wiki widget">
-		<h3 class="title"><a href="<?php echo esc_url( home_url( '/resources/' ) ); ?>">WIMP Wiki</a></h3>
-		<p>Learn from others or contribute to the collection of knowledge yourself.</p>
-	</aside>
-	<aside class="community-wimp widget">
-		<h3 class="title"><a href="<?php echo esc_url( home_url( '/community-wall/' ) ); ?>">Community Wall</a></h3>
-		<p>Ask a person, not a search engine.</p>
-	</aside>
-	<aside class="find-a-wimp widget">
-		<h3 class="title"><a href="<?php echo esc_url( home_url( '/jobs/' ) ); ?>">Find A Wimp</a></h3>
-		<p>Hire a specialist in:</p>
-		<ul>
-			<li><a href="<?php echo esc_url( home_url( '/jobs/category/business-management/' ) ); ?>">Business/Management</a></li>
-			<li><a href="<?php echo esc_url( home_url( '/jobs/category/design/' ) ); ?>">Design</a></li>
-			<li><a href="<?php echo esc_url( home_url( '/jobs/category/education/' ) ); ?>">Education</a></li>
-			<li><a href="<?php echo esc_url( home_url( '/jobs/category/programing/' ) ); ?>">Programing</a></li>
-			<li><a href="<?php echo esc_url( home_url( '/jobs/category/sales-business-dev/' ) ); ?>">Sales/Business Dev</a></li>
-			<li><a href="<?php echo esc_url( home_url( '/jobs/category/other/' ) ); ?>">Other</a></li>
-		</ul>
-	</aside>
-
-	<?php if ( ! $user_ID ) : ?>
-		<aside class="meetup widget last">
+	<aside class="widget">
+		<?php if ( ! $user_ID ) : ?>
 			<h3 class="title"><a href="<?php echo esc_url( home_url( '/members/sign-up/' ) ); ?>">Be A Wimp</a></h3>
-			<p>Join us. It’s fun!</p>
-		</aside>
-	<?php endif; ?>
+			<p><a href="<?php echo esc_url( home_url( '/members/sign-up/' ) ); ?>"</p>
+		<?php else : ?>
+			<h3 class="title"><a href="<?php echo esc_url( home_url( '/members/' ) ); ?>">I'm A Wimp!</a></h3>
+			<p><a href="<?php echo esc_url( home_url( '/members/activity/' ) ); ?>">View Member Activity</a></p>
+		<?php endif; ?>
+		<p><a href="<?php echo esc_url( home_url( '/events/' ) ); ?>">Come to a Meetup</a></p>
+	</aside>
+	<aside class="widget">
+		<h3 class="title"><a href="<?php echo esc_url( home_url( '/jobs/' ) ); ?>">Find Work</a></h3>
+		<p><a href="<?php echo esc_url( home_url( '/jobs/' ) ); ?>">Job Board</a></p>
+		<p><a href="<?php echo esc_url( home_url( '/wimp-referral-program/' ) ); ?>">Referral Program</a></p>
+	</aside>
+	<aside class="widget">
+		<h3 class="title"><a href="<?php echo esc_url( home_url( '/jobs/' ) ); ?>">Find A Wimp</a></h3>
+		<p><a href="<?php echo esc_url( home_url( '/jobs/add/' ) ); ?>">Post A Job</a></p>
+		<p><a href="<?php echo esc_url( home_url( '/wimp-referral-questionnaire/' ) ); ?>">Get Referrals</a></p>
+	</aside>
 </section>
