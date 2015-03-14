806f15dafad29ce1a8754e144190f7fc5a0988dd
Cole Geissinger <colegeissinger@gmail.com>
2015-03-14 16:44:54 -0700

    Fixing listing redirect so it doesn't depend on BuddyPress

--- listing-redirect.php
+++ listing-redirect.php
@@ -4,8 +4,7 @@
  *
  * Used for redirecting a specific user to their listing.
  */
-
-$post = wmd_get_listing_by_user_id( bp_displayed_user_id() );
+$post = wmd_get_listing_by_user_id( get_current_user_id() );
 
 wp_redirect( get_permalink( $post->ID ) );
 exit();
