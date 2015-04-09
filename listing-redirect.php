02d42084930de549034089b69742a3fc98fec95b
Cole Geissinger <colegeissinger@gmail.com>
2015-04-08 20:26:41 -0700

    Making sure the proper escaping is in place

--- listing-redirect.php
+++ listing-redirect.php
@@ -8,10 +8,10 @@ $post = wmd_get_listing_by_user_id( get_current_user_id() );
 
 if ( ! isset( $post->ID ) || empty( $post->ID ) ) {
 	echo '<p>Something went wrong and we couldn\'t load your listing!<br />';
-	echo '<a href="' . wmd_get_listing_manager_url() . '">Go back to the listing editor</a>.</p>';
+	echo '<a href="' . esc_url( wmd_get_listing_manager_url() ) . '">Go back to the listing editor</a>.</p>';
 
 	// Email us when this fails...
-	$message  = "Listing redirect failed.\n\rUser ID: " . get_current_user_id();
+	$message  = "Listing redirect failed.\n\rUser ID: " . absint( get_current_user_id() );
 	wp_mail( 'cole@beawimp.org', 'Listing redirect failed', $message );
 	die();
 }
