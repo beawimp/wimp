462beb3501819e4ba0f71d5f4e25145d26905c6d
Cole Geissinger <colegeissinger@gmail.com>
2015-05-03 20:48:24 -0700

    Adding favicon to theme

--- header.php
+++ header.php
@@ -15,6 +15,7 @@
 		<title><?php wp_title( '|', true, 'right' ); ?></title>
 		<link rel="profile" href="http://gmpg.org/xfn/11">
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+		<link rel="shortcut icon" href="<?php echo esc_url( get_stylesheet_directory_uri() . '/favicon.png' ); ?>" />
 
 		<?php wp_head(); ?>
 		<script>
