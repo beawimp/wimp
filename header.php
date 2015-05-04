854b91a5d89a5ee6c379ffd91611d85d9e267cb1
Cole Geissinger <colegeissinger@gmail.com>
2015-05-03 20:48:36 -0700

    Adding Pingdom to site

--- header.php
+++ header.php
@@ -19,6 +19,7 @@
 
 		<?php wp_head(); ?>
 		<script>
+			// Google Analytics, obviously :P
 			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -26,6 +27,17 @@
 
 			ga('create', 'UA-47611761-1', 'beawimp.org');
 			ga('send', 'pageview');
+
+			// Pingdom
+			var _prum = [['id', '553152f5abe53ddc1c46c90c'],
+				['mark', 'firstbyte', (new Date()).getTime()]];
+			(function() {
+				var s = document.getElementsByTagName('script')[0]
+					, p = document.createElement('script');
+				p.async = 'async';
+				p.src = '//rum-static.pingdom.net/prum.min.js';
+				s.parentNode.insertBefore(p, s);
+			})();
 		</script>
 	</head>
 
