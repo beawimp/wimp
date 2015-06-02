83e869e9450f286bf415885f29e5bf3f046ec497
Cole Geissinger <colegeissinger@gmail.com>
2015-06-01 21:21:19 -0700

    Adding a full width page template

---
+++ page-wide.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ * Template Name: Full Width
+ *
+ * @package wimp
+ */
+
+get_header(); ?>
+
+	<section class="main-body" class="site-content full-width" role="main">
+	
+		<?php while ( have_posts() ) : the_post(); ?>
+
+			<?php get_template_part( 'content', get_post_type() ); ?>
+
+		<?php endwhile; ?>
+
+	</section>
+
+<?php get_footer(); ?>
